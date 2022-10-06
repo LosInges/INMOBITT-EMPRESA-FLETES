f676766b5fc7d5280d26e0ad4d810178f4a11202
Jazmin Ochoa Benitez <jazmin_2303@hotmail.com>
2022-10-06 10:40:10 -0600

    Vista general de Paquetes

--- src/app/fletes/fletes-routing.module.ts
+++ src/app/fletes/fletes-routing.module.ts
@@ -21,7 +21,7 @@ const routes: Routes = [
     component: PrecargaComponent,
   },
   {
-    path: 'paquetes',
+    path: ':id/paquetes',
     component: PaquetesComponent,
     children: [
       {
