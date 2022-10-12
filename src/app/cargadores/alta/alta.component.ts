6d617f061409bfe30625afce1131e3864cf916bb
Juan Jesus Mata Solis <jujemataso@ittepic.edu.mx>
2022-10-12 10:37:02 -0600

    cambios

--- src/app/cargadores/alta/alta.component.ts
+++ src/app/cargadores/alta/alta.component.ts
@@ -1,14 +1,59 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { NavigationEnd, Router } from '@angular/router';
+import { Cargador } from 'src/app/fletes/interfaces/cargador';
+import { CargadoresService } from 'src/app/fletes/services/cargadores.service';
 
 @Component({
   selector: 'app-alta',
   templateUrl: './alta.component.html',
   styleUrls: ['./alta.component.scss'],
 })
-export class AltaComponent implements OnInit {
+export class AltaComponent implements OnInit, OnDestroy {
+  apellido1 = '';
+  apellido2 = '';
+  eventosRouter: any;
+  cargador: Cargador = {
+    rfc: '',
+    nombre: '',
+    apellido: '',
+    password: '',
+    telefono: '',
+    foto: '',
+    empresa: 'empresa@mail.com',
+  };
 
-  constructor() { }
+  constructor(
+    private cargadoresService: CargadoresService,
+    private router: Router
+  ) {
+    this.eventosRouter = this.router.events.subscribe((val) => {
+      if (val instanceof NavigationEnd) {
+        this.ngOnInit();
+      }
+    });
+  }
 
   ngOnInit() {}
 
+  ngOnDestroy(): void {
+    if (this.eventosRouter) {
+      this.eventosRouter.unsubscribe();
+    }
+  }
+
+  registrarCargador() {
+    this.cargador.apellido = this.apellido1 + ' ' + this.apellido2;
+    this.cargadoresService.postCargador(this.cargador).subscribe((res) => {
+      this.cargador = {
+        rfc: '',
+        nombre: '',
+        apellido: '',
+        password: '',
+        telefono: '',
+        foto: '',
+        empresa: 'empresa@mail.com',
+      };
+      this.router.navigate(['/cargadores']);
+    });
+  }
 }
--- src/app/cargadores/cargadores.page.ts
+++ src/app/cargadores/cargadores.page.ts
@@ -1,15 +1,39 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { NavigationEnd, Router } from '@angular/router';
+import { Cargador } from '../fletes/interfaces/cargador';
+import { CargadoresService } from '../fletes/services/cargadores.service';
 
 @Component({
   selector: 'app-cargadores',
   templateUrl: './cargadores.page.html',
   styleUrls: ['./cargadores.page.scss'],
 })
-export class CargadoresPage implements OnInit {
+export class CargadoresPage implements OnInit, OnDestroy {
+  eventosRouter: any;
+  cargadores: Cargador[] = [];
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor(
+    private router: Router,
+    private cargadoresService: CargadoresService
+  ) {
+    this.eventosRouter = this.router.events.subscribe((val) => {
+      if (val instanceof NavigationEnd) {
+        this.ngOnInit();
+      }
+    });
   }
 
+  ngOnInit() {
+    this.cargadoresService
+      .getCargadores('empresa@mail.com')
+      .subscribe((cargadores) => {
+        this.cargadores = cargadores;
+      });
+  }
+
+  ngOnDestroy(): void {
+    if (this.eventosRouter) {
+      this.eventosRouter.unsubscribe();
+    }
+  }
 }
