64a7c6f9c55ad4a477d3bdfd01253617e02f845b
Jazmin Ochoa Benitez <jazmin_2303@hotmail.com>
2022-10-21 22:25:45 -0600

    flete modules conflictos

--- src/app/fletes/paquetes/info-paquetes/info-paquetes.component.ts
+++ src/app/fletes/paquetes/info-paquetes/info-paquetes.component.ts
@@ -2,6 +2,7 @@ import { Component, Input, OnInit } from '@angular/core';
 import { ModalController } from '@ionic/angular';
 import { Cargador } from '../../interfaces/cargador';
 import { Transporte } from '../../interfaces/transporte';
+import { CargadoresService } from '../../services/cargadores.service';
 import { TransporteFleteService } from '../../services/transporte-flete.service';
 @Component({
   selector: 'app-info-paquetes',
@@ -9,37 +10,45 @@ import { TransporteFleteService } from '../../services/transporte-flete.service'
   styleUrls: ['./info-paquetes.component.scss'],
 })
 export class InfoPaquetesComponent implements OnInit {
-  @Input() flete: string
-  cargadores: string[] = [];
-  transporte: string
-  cargadoresEmpresa: string[] = []
+  @Input() flete: string;
+  cargadores: Cargador[] = [];
+  transporte: string;
+  cargadoresEmpresa: Cargador[] = [];
 
   constructor(
     private modalController: ModalController,
-    private transporteFletesService: TransporteFleteService
-  ) { }
-
+    private transporteFletesService: TransporteFleteService,
+    private cargadoresService: CargadoresService
+  ) {}
 
   ngOnInit() {
     this.transporteFletesService
-    .getTransportesFlete(this.flete).subscribe((transporteFlete)=>{
-      this.transporte = transporteFlete.transporte;
-      this.cargadores = transporteFlete.cargadores
-    });
+      .getTransportesFlete(this.flete)
+      .subscribe((transporteFlete) => {
+        this.transporte = transporteFlete.transporte;
+        transporteFlete.cargadores.forEach((cargador) => {
+          if (this.cargadores)
+            this.cargadoresService
+              .getCargador('empresa@mail.com', cargador)
+              .subscribe((c) => (this.cargadores = [c]));
+          else
+            this.cargadoresService
+              .getCargador('empresa@mail.com', cargador)
+              .subscribe((c) => this.cargadores.push(c));
+        });
+      });
+
+      this.cargadoresService.getCargadores('empresa@mail.com').subscribe((c) =>
+        this.cargadoresEmpresa = c
+
+      )
   }
 
-  cerrar(){
-    return this.modalController.dismiss()
+  cerrar() {
+    return this.modalController.dismiss();
   }
 
+  eliminar(cargador: string) {}
 
-
-  eliminar(cargador: string){
-
-  }
-
-  agregar(){
-
-  }
-
+  agregar() {}
 }
