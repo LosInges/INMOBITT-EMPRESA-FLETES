9a647ecb71e19de9ad2da3fa211ca161d0f947fd
Jazmin Ochoa Benitez <jazmin_2303@hotmail.com>
2022-11-20 11:20:51 -0700

    Validación de correo. Forma no lógica, impleementando longin Service

--- src/app/login/signup/signup.component.ts
+++ src/app/login/signup/signup.component.ts
@@ -1,8 +1,10 @@
+import { AlertController, ModalController } from '@ionic/angular';
 import { Component, OnInit } from '@angular/core';
-import { ModalController, AlertController} from '@ionic/angular';
+
 import { Empresa } from 'src/app/fletes/interfaces/empresa';
 import { EmpresaService } from 'src/app/fletes/services/empresa.service';
 import { EstadosService } from 'src/app/services/estados.service';
+import { LoginService } from 'src/app/fletes/services/login.service';
 import { Router } from '@angular/router';
 
 @Component({
@@ -10,7 +12,6 @@ import { Router } from '@angular/router';
   templateUrl: './signup.component.html',
   styleUrls: ['./signup.component.scss'],
 })
-
 export class SignupComponent implements OnInit {
   empresa: Empresa = {
     nombre: '',
@@ -28,20 +29,21 @@ export class SignupComponent implements OnInit {
     private modalController: ModalController,
     private alertCtrl: AlertController,
     private router: Router,
-  ) { }
+    private loginService: LoginService
+  ) {}
 
-  async mostrarAlerta(titulo:string, subtitulo:string, mensaje:string) {  
-    const alert = await this.alertCtrl.create({  
-      header: titulo,  
-      subHeader: subtitulo,  
-      message: mensaje,  
-      buttons: ['OK']  
-    });  
-    await alert.present();  
-    const result = await alert.onDidDismiss();  
-    console.log(result);  
-    this.router.navigate(['/', 'login'])
-  } 
+  async mostrarAlerta(titulo: string, subtitulo: string, mensaje: string) {
+    const alert = await this.alertCtrl.create({
+      header: titulo,
+      subHeader: subtitulo,
+      message: mensaje,
+      buttons: ['OK'],
+    });
+    await alert.present();
+    const result = await alert.onDidDismiss();
+    console.log(result);
+    this.router.navigate(['/', 'login']);
+  }
 
   ngOnInit() {
     console.log(this.estados);
@@ -49,26 +51,52 @@ export class SignupComponent implements OnInit {
 
   onSubmit() {
     if (
-      this.empresa.nombre.trim().length <= 0 ||
-      this.empresa.correo.trim().length <= 0 ||
-      this.empresa.password.trim().length <= 0 ||
-      this.empresa.telefono.trim().length <= 0 ||
-      this.empresa.estados.length <= 0
+      this.empresa.nombre.trim().length > 0 ||
+      this.empresa.correo.trim().length > 0 ||
+      this.empresa.password.trim().length > 0 ||
+      this.empresa.telefono.trim().length > 0 ||
+      this.empresa.estados.length > 0
     ) {
-      this.mostrarAlerta("Error", "Campos vacios", "No deje espacios en blanco.")
-    }else{
       if (this.confirmPassword === this.empresa.password) {
-        this.empresaService.postEmpresa(this.empresa).subscribe((res) => {
-          console.log(res);
-          this.mostrarAlerta("Completado", "Creación", "Cliente creado exitosamente.")
-          this.cerrar()
-        });
-      }else{
-        this.mostrarAlerta("Error:", "Confirmación de clave incorrecta", "¿es correcta o esta vacia?")
+        this.loginService
+          .solicitarRegistro(this.empresa.correo)
+          .subscribe((solicitud) => {
+            if (solicitud.permiso) {
+              this.empresaService.postEmpresa(this.empresa).subscribe((res) => {
+                console.log(res);
+                this.mostrarAlerta(
+                  'Completado',
+                  'Creación',
+                  'Cliente creado exitosamente.'
+                );
+                this.cerrar();
+              });
+            } else {
+              this.mostrarAlerta(
+                'Error:',
+                'Correo ya registrado',
+                'Favor de introducir otro correo.'
+              );
+            }
+          });
+      } else {
+        this.mostrarAlerta(
+          'Error:',
+          'Confirmación de clave incorrecta',
+          '¿es correcta o esta vacia?'
+        );
       }
+    } else {
+      this.mostrarAlerta(
+        'Error',
+        'Campos vacios',
+        'No deje espacios en blanco.'
+      );
+      console.log(this.empresa);
+
     }
   }
   cerrar() {
-    this.modalController.dismiss()
+    this.modalController.dismiss();
   }
 }
